3ce2ef397b3964dd0ba9c07a2009a0a10748645e
Eugene Bevziuk <bevziukeuge@icloud.com>
2018-10-01 15:33:08 +0300

    Install wordpress

--- assets/js/formSend.js
+++
@@ -1,4 +0,0 @@
-var wpcf7ElmAnother = document.querySelector( 'section#form div.wpcf7' );
-wpcf7ElmAnother.addEventListener( 'wpcf7mailsent', function( event ) {
-    jQuery("#successfully").modal();
-}, false );
--- assets/js/formSendModal.js
+++
@@ -1,4 +0,0 @@
-var wpcf7Elm = document.querySelector( 'div#leaveAppli div.wpcf7' );
-wpcf7Elm.addEventListener( 'wpcf7mailsent', function( event ) {
-    jQuery("#leaveAppli").find('.main-cont').toggleClass('hidden') && jQuery("#leaveAppli").find('.sub-cont').toggleClass('hidden') && jQuery("#leaveAppli").find('.wpcf7').hide();
-}, false );
