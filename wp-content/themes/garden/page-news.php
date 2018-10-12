b0c6e29afbffec2dd73cd0df0f8f7780ddb0dcd1
Eugene Bevziuk <bevziukeuge@icloud.com>
2018-10-12 10:52:41 +0300

    Html page news

---
+++ wp-content/themes/garden/page-news.php
@@ -0,0 +1,256 @@
+<?php
+/*
+Template Name: News
+*/
+?>
+
+<?php get_header(); ?>
+
+    <section class="news-bg">
+        <img src="<?php echo get_template_directory_uri(); ?>/build/images/news/news-bg.jpg" alt="">
+        <h2 class="news-bg__name">News</h2>
+    </section>
+
+    <section class="birthday birthday_p">
+
+        <div class="container">
+
+            <h2 class="news-name news-name_p">Именинники месяца</h2>
+
+            <div class="row align-items-center">
+                
+                <div class="col-lg-6">
+                    <div class="birthday-item">
+
+                        <div class="row">
+
+                            <div class="col-lg-4">
+                                <div class="birthday-item__img">
+                                    <img src="<?php echo get_template_directory_uri(); ?>/build/images/news/birthday.jpg" alt="" class="img-fluid">
+                                </div>
+                            </div>
+                            <div class="col-lg-8">
+
+                                <div class="birthday-item__text-wrp">
+
+                                    <div class="birthday-item__title">Поздравляем</div>
+                                    <div class="birthday-item__name">Иванова Ивановича</div>
+                                    <div class="birthday-item__text">
+                                        Тебе желаю море счастья,
+                                        Улыбок, солнца и тепла.
+                                        Чтоб жизнь была еще прекрасней,
+                                        Удача за руку вела!
+                                    </div>
+
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="birthday-item">
+
+                        <div class="row">
+
+                            <div class="col-lg-4">
+                                <div class="birthday-item__img">
+                                    <img src="<?php echo get_template_directory_uri(); ?>/build/images/news/birthday.jpg" alt="" class="img-fluid">
+                                </div>
+                            </div>
+                            <div class="col-lg-8">
+
+                                <div class="birthday-item__text-wrp">
+
+                                    <div class="birthday-item__title">Поздравляем</div>
+                                    <div class="birthday-item__name">Иванова Ивановича</div>
+                                    <div class="birthday-item__text">
+                                        Тебе желаю море счастья,
+                                        Улыбок, солнца и тепла.
+                                        Чтоб жизнь была еще прекрасней,
+                                        Удача за руку вела!
+                                    </div>
+
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+    </section>
+
+    <section class="event-slider-wrp event-slider-wrp_p">
+
+        <h2 class="news-name news-name_p">Поздравление с праздниками</h2>
+
+        <div class="event-slider">
+
+            <div class="event-slide">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="event-slide__text-wrp">
+                                <div class="event-slide__title">Поздравляем Вас С Новым 2019 годом</div>
+                                <div class="event-slide__text">
+                                    Желаю в этот Новый год
+                                    Поменьше грусти и забот,
+                                    Побольше счастья и добра,
+                                    Улыбок, нежности, тепла!
+
+                                    Чтоб были верными друзья
+                                    И очень дружною семья,
+                                    Чтоб каждый день удачным был,
+                                    И чтоб на всё хватало сил!
+
+                                    Ну а ещё пусть Новый год
+                                    Побольше денег принесёт,
+                                    Здоровья, мира и любви,
+                                    Чтоб в сердце не было зимы!
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="event-slide__img">
+                                <img src="<?php echo get_template_directory_uri(); ?>/build/images/news/news-slide.jpg" alt="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="event-slide">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="event-slide__text-wrp">
+                                <div class="event-slide__title">Поздравляем Вас С Новым 2019 годом</div>
+                                <div class="event-slide__text">
+                                    Желаю в этот Новый год
+                                    Поменьше грусти и забот,
+                                    Побольше счастья и добра,
+                                    Улыбок, нежности, тепла!
+
+                                    Чтоб были верными друзья
+                                    И очень дружною семья,
+                                    Чтоб каждый день удачным был,
+                                    И чтоб на всё хватало сил!
+
+                                    Ну а ещё пусть Новый год
+                                    Побольше денег принесёт,
+                                    Здоровья, мира и любви,
+                                    Чтоб в сердце не было зимы!
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="event-slide__img">
+                                <img src="<?php echo get_template_directory_uri(); ?>/build/images/news/news-slide.jpg" alt="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="event-slide">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="event-slide__text-wrp">
+                                <div class="event-slide__title">Поздравляем Вас С Новым 2019 годом</div>
+                                <div class="event-slide__text">
+                                    Желаю в этот Новый год
+                                    Поменьше грусти и забот,
+                                    Побольше счастья и добра,
+                                    Улыбок, нежности, тепла!
+
+                                    Чтоб были верными друзья
+                                    И очень дружною семья,
+                                    Чтоб каждый день удачным был,
+                                    И чтоб на всё хватало сил!
+
+                                    Ну а ещё пусть Новый год
+                                    Побольше денег принесёт,
+                                    Здоровья, мира и любви,
+                                    Чтоб в сердце не было зимы!
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="event-slide__img">
+                                <img src="<?php echo get_template_directory_uri(); ?>/build/images/news/news-slide.jpg" alt="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="event-slide">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="event-slide__text-wrp">
+                                <div class="event-slide__title">Поздравляем Вас С Новым 2019 годом</div>
+                                <div class="event-slide__text">
+                                    Желаю в этот Новый год
+                                    Поменьше грусти и забот,
+                                    Побольше счастья и добра,
+                                    Улыбок, нежности, тепла!
+
+                                    Чтоб были верными друзья
+                                    И очень дружною семья,
+                                    Чтоб каждый день удачным был,
+                                    И чтоб на всё хватало сил!
+
+                                    Ну а ещё пусть Новый год
+                                    Побольше денег принесёт,
+                                    Здоровья, мира и любви,
+                                    Чтоб в сердце не было зимы!
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="event-slide__img">
+                                <img src="<?php echo get_template_directory_uri(); ?>/build/images/news/news-slide.jpg" alt="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </section>
+
+    <section class="calender-wrp calender-wrp_p">
+
+        <h2 class="news-name news-name_p">Мероприятия</h2>
+
+        <div class="container">
+            <div class="row">
+
+                <div class="col-lg-8">
+
+                    <div class="calender-bg">
+                        <img src="<?php echo get_template_directory_uri(); ?>/build/images/news/news-event.jpg" alt="">
+                        <div class="calender-bg__title">Наши мероприятия</div>
+                    </div>
+
+                </div>
+                <div class="col-lg-4">
+                    <div class="calender">
+                        <img src="<?php echo get_template_directory_uri(); ?>/build/images/news/calender.png" alt="">
+                    </div> 
+                </div>
+
+            </div>
+        </div>
+
+    </section>
+
+<?php get_footer(); ?>
